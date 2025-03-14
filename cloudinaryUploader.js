2428da93df950b6c602a544f198cd1ec683beeda
JJ-practice <fixymefixyou@gmail.com>
2025-03-14 10:05:09 +0800

    move cloudinary key to env

--- cloudinaryUploader.js
+++ cloudinaryUploader.js
@@ -1,12 +1,18 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: "drozrybph",
-  api_key: "***REMOVED***",
-  api_secret: "***REMOVED***",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function uploadImageToCloudinary(imageUrl) {
-  const result = await cloudinary.v2.uploader.upload(imageUrl);
+  const result = await cloudinary.v2.uploader.upload(imageUrl, {
+    folder: "Education",
+  });
   return result.secure_url;
 }
